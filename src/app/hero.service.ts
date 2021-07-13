d25ceceffcb14c97f4147c1cbca0471ab212b6ea
Jacob Williams <jacobwilliams@Jacobs-MacBook-Pro.local>
2021-07-13 18:13:49 -0400

    part 5

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HeroesComponent } from './heroes/heroes.component';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 }
